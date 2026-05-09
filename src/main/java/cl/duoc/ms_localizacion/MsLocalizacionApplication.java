2522425c62124017a23bbc9d7dd48ac4b7de647e
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 17:06:46 -0400

    Cesar
    Primer Commit

---
+++ src/main/java/cl/duoc/ms_localizacion/MsLocalizacionApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_localizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLocalizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLocalizacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_localizacion/MsLocalizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_localizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLocalizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
