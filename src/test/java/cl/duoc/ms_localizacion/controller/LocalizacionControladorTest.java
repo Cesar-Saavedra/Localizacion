ef983bd8371906e2b197e8f8fca0417fea00be36
TomasB <to.blanchard@duocuc.cl>
2026-06-30 15:42:43 -0400

    arreglo de bugs
    no compilaba proyecto por falta de compatibilidad

--- src/test/java/cl/duoc/ms_localizacion/controller/LocalizacionControladorTest.java
+++ src/test/java/cl/duoc/ms_localizacion/controller/LocalizacionControladorTest.java
@@ -10,7 +10,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,6 +28,7 @@ import cl.duoc.ms_localizacion.service.LocalizacionServicio;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(LocalizacionControlador.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class LocalizacionControladorTest {
 
     @Autowired
